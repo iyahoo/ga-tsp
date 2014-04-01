020fbb54a1399a272cc98ecc9a3d90e3eb527bd8
iyahoo <s1200191@gmail.com>
2014-04-02 01:11:54 +0900

    progress

--- ga-tsp.lisp
+++ ga-tsp.lisp
@@ -13,66 +13,67 @@
 (defun gene-code ()
   "重複のないランダムな1-10の遺伝子をつくる"
   (let ((genes))
-	(loop for i
-	   while (< (length genes) 10)	 
-	   do (let ((num (1+ (random 10))))
-			(unless (member num genes)
-			  (push num genes))))
-	genes))
+    (loop for i
+       while (< (length genes) 10)	 
+       do (let ((num (1+ (random 10))))
+            (unless (member num genes)
+              (push num genes))))
+    genes))
 
 (defun not-random-gene-code ()
   (let ((genes)
-		(*random-state* (make-random-state nil)))
-	(loop for i
-	   while (< (length genes) 10)	 
-	   do (let ((num (1+ (random 10))))
-			(unless (member num genes)
-			  (push num genes))))
-	genes))
+        (*random-state* (make-random-state nil)))
+    (loop for i
+       while (< (length genes) 10)	 
+       do (let ((num (1+ (random 10))))
+            (unless (member num genes)
+              (push num genes))))
+    genes))
 
 (defun random-node ()
   (1+ (random +city-number+)))
 
 (defun edge-pair (a b)
   (unless (eql a b)
-	(list (cons a b) (cons b a))))
+    (list (cons a b) (cons b a))))
 
 (defun make-edge-list ()
   (apply #'append (loop repeat +edge-num+
-					 collect (edge-pair (random-node) (random-node)))))
+                     collect (edge-pair (random-node) (random-node)))))
 
 (defun direct-edges (node edge-list)
   (remove-if-not #'(lambda (x)
-					 (eql (car x) node))
-				 edge-list))
+                     (eql (car x) node))
+                 edge-list))
 
 (defun get-conneced (node edge-list)
   (let ((visited))
-	(labels ((traverse (node)
-			   (unless (member node visited)
-				 (push node visited)
-				 (mapc #'(lambda (edge)
-						   (traverse (cdr edge)))
-					   (direct-edges node edge-list)))))
-	  (traverse node))
-	visited))
+    (labels ((traverse (node)
+               (unless (member node visited)
+                 (push node visited)
+                 (mapc #'(lambda (edge)
+                           (traverse (cdr edge)))
+                       (direct-edges node edge-list)))))
+      (traverse node))
+    visited))
 
 (defun find-islands (nodes edge-list)
   (let ((islands))
-	(labels ((find-island (nodes)
-			   (let* ((connected (get-conneced (car nodes) edge-list))
-					  (unconnected (set-difference nodes connected)))
-				 (push connected islands)
-				 (when unconnected
-				   (find-island unconnected)))))
-	  (find-island nodes))
-	islands))
+    (labels ((find-island (nodes)
+               (let* ((connected (get-conneced (car nodes) edge-list))
+                      (unconnected (set-difference nodes connected)))
+                 (push connected islands)
+                 (when unconnected
+                   (find-island unconnected)))))
+      (find-island nodes))
+    islands))
 
 (defun connect-with-bridges (islands)
   (when (cdr islands)
-	(append (edge-pair (caar islands) (caddr islands))
-			(connect-with-bridges (cdr islands)))))
+    (append (edge-pair (caar islands) (caddr islands))
+            (connect-with-bridges (cdr islands)))))
 
 (defun connect-all-islands (nodes edge-list)
   (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
+
 
